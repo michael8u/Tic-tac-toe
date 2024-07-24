88801eb870089d4cc105217120194c6b3783961d
Michael <165772955+michael8u@users.noreply.github.com>
2024-07-24 20:16:23 +0300

    Delete scripts directory

--- scripts/dev.js
+++
@@ -1,7 +0,0 @@
-(async function() {
-    const open = require('open');
-
-    let session = await open('http://localhost:8080');
-
-    console.log('Open Feedback: Development environment opened in browser ✅');
-})();
